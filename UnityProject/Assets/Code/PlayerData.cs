03307e2ad5334dd84b52482de55ff098799c3909
zhouxin <609195107@qq.com>
2017-12-09 11:02:41 +0800

    ccc

---
+++ UnityProject/Assets/Code/PlayerData.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerData : NetworkBehaviour
+{
+	public static PlayerData Create (GameObject player)
+	{
+		PlayerData pd = player.GetComponent<PlayerData> ();
+		if (pd == null) {
+			pd = player.AddComponent <PlayerData> ();	
+		}
+		return pd;
+	}
+
+	[SyncVar]
+	public int hp = 100;
+	[SyncVar]
+	public int skill = 1;
+	[SyncVar]
+	public Vector3 synsPos;
+
+	void FixedUpdate ()
+	{
+		TransmitPosition ();
+		LerpPosition ();
+	}
+
+	void LerpPosition ()
+	{
+		if (!isLocalPlayer) {
+			transform.position = Vector3.Lerp (transform.position, synsPos, Time.deltaTime);
+		}
+	}
+
+	[Command]
+	void CmdProvidePositionToServer (Vector3 pos)
+	{
+		synsPos = pos;
+	}
+
+	[ClientCallback]
+	void TransmitPosition ()
+	{
+		if (isLocalPlayer) {
+			CmdProvidePositionToServer (transform.position);
+		}
+	}
+}
---
+++ UnityProject/Assets/Code/TestNet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TestNet : NetworkBehaviour
+{
+	[SyncVar]
+	public int testA;
+
+	// Use this for initialization
+	void Start ()
+	{
+//		NetworkManager nm;
+//		nm.StartServer ();
+//		NetworkClient nc = nm.StartHost ();
+//		NetworkClient sc = nm.StartClient ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
