c369474be57e584332ede9a319466e07c6daab45
zhouxin <609195107@qq.com>
2017-12-09 10:17:01 +0800

    ccc

--- UnityProject/Assets/Scripts/Level/GrassLand.cs
+++ UnityProject/Assets/Scripts/Level/GrassLand.cs
@@ -70,8 +70,8 @@ public class GrassLand : MonoBehaviour
 
 	public UISprite spritePrefab;
 
-	private Dictionary<LandType, LandSprite> _landSprites = new Dictionary<LandType, LandSprite>();
-	private Dictionary<ItemType, ItemSprite> _itemSprites = new Dictionary<ItemType, ItemSprite>();
+	private Dictionary<LandType, LandSprite> _landSprites = new Dictionary<LandType, LandSprite> ();
+	private Dictionary<ItemType, ItemSprite> _itemSprites = new Dictionary<ItemType, ItemSprite> ();
 	private UIRoot _root;
 
 
@@ -85,36 +85,35 @@ public class GrassLand : MonoBehaviour
 		public LandType landType;
 		public ItemType itemType;
 	}
+
 	private Tile[][] tiles;
 
-	void Awake()
+	void Awake ()
 	{
-		foreach (var landSprite in landSprites)
-		{
-			_landSprites.Add(landSprite.type, landSprite);
+		foreach (var landSprite in landSprites) {
+			_landSprites.Add (landSprite.type, landSprite);
 		}
 
-		foreach (var itemSprite in itemSprites)
-		{
-			_itemSprites.Add(itemSprite.type, itemSprite);
+		foreach (var itemSprite in itemSprites) {
+			_itemSprites.Add (itemSprite.type, itemSprite);
 		}
 
-		_root = UIRoot.list[0];
+		_root = UIRoot.list [0];
 	}
 
-	void Start()
+	void Start ()
 	{
-		GenerateTile();
+		GenerateTile ();
 	}
 
-	void GenerateTile()
+	void GenerateTile ()
 	{
 
 		tiles = new Tile[rows][];
 
-		GameObject emptyLayer = new GameObject("Empty");
-		GameObject itemLayer = new GameObject("Item");
-		GameObject landLayer = new GameObject("Land");
+		GameObject emptyLayer = new GameObject ("Empty");
+		GameObject itemLayer = new GameObject ("Item");
+		GameObject landLayer = new GameObject ("Land");
 		emptyLayer.transform.parent = _root.transform;
 		itemLayer.transform.parent = _root.transform;
 		landLayer.transform.parent = _root.transform;
@@ -123,70 +122,66 @@ public class GrassLand : MonoBehaviour
 		landLayer.transform.localScale = Vector3.one;
 
 		// Empty Tile
-		for (int r = 0; r < rows; r++)
-		{
-			tiles[r] = new Tile[columns];
-			for (int c = 0; c < columns; c++)
-			{
-				UISprite uisprite = MakeSprite(emptyLayer.transform, _landSprites[LandType.Soil].sprite, r, c, 100);
+		for (int r = 0; r < rows; r++) {
+			tiles [r] = new Tile[columns];
+			for (int c = 0; c < columns; c++) {
+				UISprite uisprite = MakeSprite (emptyLayer.transform, _landSprites [LandType.Soil].sprite, r, c, 100);
 
 				Tile t;
-				t = new Tile();
+				t = new Tile ();
 				t.row = r;
 				t.column = c;
 				t.landType = LandType.Soil;
 				t.itemType = ItemType.None;
 				t.emptySprite = uisprite;
-				tiles[r][c] = t;
+				tiles [r] [c] = t;
 			}
 		}
 
 		// Item Tile
-		foreach (GameItem item in items)
-		{
-			foreach (Int2 area in item.areas)
-			{
-				UISprite uisprite = MakeSprite(itemLayer.transform, _itemSprites[item.type].sprite, area.row, area.column, 200);
-				Tile t = tiles[area.row][area.column];
+		foreach (GameItem item in items) {
+			foreach (Int2 area in item.areas) {
+				UISprite uisprite = MakeSprite (itemLayer.transform, _itemSprites [item.type].sprite, area.row, area.column, 200);
+				Tile t = tiles [area.row] [area.column];
 				t.itemType = item.type;
 				t.itemSprite = uisprite;
 			}
 		}
 
 		// Land Tile
-		foreach (GameLand land in lands)
-		{
-			foreach (Int2 area in land.areas)
-			{
-				UISprite uisprite = MakeSprite(landLayer.transform, _landSprites[land.type].sprite, area.row, area.column, 300);
-				Tile t = tiles[area.row][area.column];
+		foreach (GameLand land in lands) {
+			foreach (Int2 area in land.areas) {
+				UISprite uisprite = MakeSprite (landLayer.transform, _landSprites [land.type].sprite, area.row, area.column, 300);
+				Tile t = tiles [area.row] [area.column];
 				t.landType = land.type;
 				t.landSprite = uisprite;
-				NGUITools.AddWidgetCollider(t.landSprite.gameObject);
+				NGUITools.AddWidgetCollider (t.landSprite.gameObject);
 			}
 		}
 	}
 
-	UISprite MakeSprite(Transform parent, string spriteName, int row, int column, int depth){
-		UISprite uisprite = Instantiate(spritePrefab, parent);
+	UISprite MakeSprite (Transform parent, string spriteName, int row, int column, int depth)
+	{
+		UISprite uisprite = Instantiate (spritePrefab);
+		uisprite.transform.SetParent (parent, false);
 
 		uisprite.atlas = atlas;
 		uisprite.spriteName = spriteName;
 		uisprite.pivot = UIWidget.Pivot.BottomLeft;
 		uisprite.depth = depth - row;
 
-		SetSpriteSize(uisprite);
+		SetSpriteSize (uisprite);
 
-		uisprite.transform.localPosition = new Vector3(column * tileSize - Screen.width / 2, row * tileSize - Screen.height / 2, 0);
+		uisprite.transform.localPosition = new Vector3 (column * tileSize - Screen.width / 2, row * tileSize - Screen.height / 2, 0);
 		return uisprite;
 	}
 
-	void SetSpriteSize(UISprite uisprite)
+	void SetSpriteSize (UISprite uisprite)
 	{
-		float aspect = (float)uisprite.GetAtlasSprite().height / (float)uisprite.GetAtlasSprite().width;
+		float aspect = (float)uisprite.GetAtlasSprite ().height / (float)uisprite.GetAtlasSprite ().width;
 		uisprite.aspectRatio = 1 / aspect;
 		uisprite.width = tileSize;
-		uisprite.height = (int) (tileSize * aspect);
+		uisprite.height = (int)(tileSize * aspect);
 	}
 
 }
