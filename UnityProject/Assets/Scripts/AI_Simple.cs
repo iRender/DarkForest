cd6a4e0fe17cb2c8b80dbde13a58db5c9e3149a6
Felix Hu <hucfelix@gmail.com>
2017-12-10 02:05:37 +0800

    - hc

---
+++ UnityProject/Assets/Scripts/AI_Simple.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Simple : MonoBehaviour {
+
+	public GrassLand m_grassL;
+	// Use this for initialization
+	void Start () {
+		Role role = GetComponent<Role> ();
+		role.m_initMoveSpeed = 200;
+		Vector2 lbPos = m_grassL.transform.localPosition;
+		role.MoveTo (lbPos, () => {
+			
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
