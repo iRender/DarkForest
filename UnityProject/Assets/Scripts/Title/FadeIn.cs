4bb7a7bb9098bfbf5216298ff180bdfdf436cf5b
yefei <mail@yefei.co>
2017-12-10 11:06:18 +0800

    Update

---
+++ UnityProject/Assets/Scripts/Title/FadeIn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+	public float alphaMax = 1.0f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		
+	}
+}
