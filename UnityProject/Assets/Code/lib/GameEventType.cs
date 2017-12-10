e2a645a6852a6938d7c4e0bad31fa44e19c8f0dc
Felix Hu <hucfelix@gmail.com>
2017-12-10 12:12:11 +0800

    - hc

--- UnityProject/Assets/Code/lib/GameEventType.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class GameEventType
-{
-	public static string Role_InGrass = "GameEventType_Role_InGrass";
-	public static string Role_OutGrass = "GameEventType_Role_OutGrass";
-}
